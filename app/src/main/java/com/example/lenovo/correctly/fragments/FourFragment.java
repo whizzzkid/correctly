e3b3c09fa16ab86cc1fd34f0e7d45e0822633164
Nsalavat <salavat@cs.toronto.edu>
2016-11-20 01:25:30 -0500

    From fragment five to fragment four
    I took your code from fragment five and added it to the fourfragment.
    cannot get information from speech recognition though

--- app/src/main/java/com/example/lenovo/correctly/fragments/FourFragment.java
+++ app/src/main/java/com/example/lenovo/correctly/fragments/FourFragment.java
@@ -1,10 +1,16 @@
 package com.example.lenovo.correctly.fragments;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
@@ -12,19 +18,32 @@ import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.lenovo.correctly.MainActivity;
 import com.example.lenovo.correctly.R;
+import com.example.lenovo.correctly.clients.StreamingRecognizeClient;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.security.ProviderInstaller;
+import com.google.protobuf.MessageOrBuilder;
+import com.google.protobuf.TextFormat;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Locale;
+
+import io.grpc.ManagedChannel;
 
 
 public class FourFragment extends Fragment {
@@ -45,6 +64,20 @@ public class FourFragment extends Fragment {
     public int i=0;
     public String[] list_of_wordsFrench = new String[]{"Bonjour", "Bonsoir", "Je m’appelle", "Je suis", "Une banque", "Magasin", "L’aeroport", "Merci", "Une voiture", "Carte de crédit"};
 
+    private static final String HOSTNAME = "speech.googleapis.com";
+    private static final int PORT = 443;
+    private static final int RECORDER_SAMPLERATE = 16000;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+
+    private AudioRecord mAudioRecord = null;
+    private Thread mRecordingThread = null;
+    private boolean mIsRecording = false;
+    private ImageButton mRecordingBt;
+    private TextView mConsoleMsg;
+    private StreamingRecognizeClient mStreamingClient;
+    private int mBufferSize;
+
 
     public String[] list_of_wordsEnglish=new String[]{"Good morning" ,
             "Good evening" ,
@@ -60,6 +93,97 @@ public class FourFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+    private void startRecording() {
+        mAudioRecord.startRecording();
+        mIsRecording = true;
+        mRecordingThread = new Thread(new Runnable() {
+            public void run() {
+                readData();
+            }
+        }, "AudioRecorder Thread");
+        mRecordingThread.start();
+    }
+
+    private void readData() {
+        byte sData[] = new byte[mBufferSize];
+        while (mIsRecording) {
+            int bytesRead = mAudioRecord.read(sData, 0, mBufferSize);
+            if (bytesRead > 0) {
+                try {
+                    mStreamingClient.recognizeBytes(sData, bytesRead);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e(getClass().getSimpleName(), "Error while reading bytes: " + bytesRead);
+            }
+        }
+    }
+
+    private void initialize() {
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg != null) {
+                    mConsoleMsg.setText(TextFormat.printToString(
+                            (MessageOrBuilder) msg.obj) + mConsoleMsg.getText());
+                }
+                super.handleMessage(msg);
+            }
+        };
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                // Required to support Android 4.x.x (patches for OpenSSL from Google-Play-Services)
+                try {
+                    ProviderInstaller.installIfNeeded(getContext());
+                } catch (GooglePlayServicesRepairableException e) {
+
+                    // Indicates that Google Play services is out of date, disabled, etc.
+                    e.printStackTrace();
+                    // Prompt the user to install/update/enable Google Play services.
+                    GooglePlayServicesUtil.showErrorNotification(
+                            e.getConnectionStatusCode(), getContext());
+                    return;
+
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // Indicates a non-recoverable error; the ProviderInstaller is not able
+                    // to install an up-to-date Provider.
+                    e.printStackTrace();
+                    return;
+                }
+
+                try {
+                    InputStream credentials = getActivity().getAssets().open
+                            ("credentials.json");
+                    ManagedChannel channel = StreamingRecognizeClient.createChannel(
+                            HOSTNAME, PORT, credentials);
+                    mStreamingClient = new StreamingRecognizeClient(channel,
+                            RECORDER_SAMPLERATE, handler);
+                } catch (Exception e) {
+                    Log.e(MainActivity.class.getSimpleName(), "Error", e);
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mStreamingClient != null) {
+            try {
+                mStreamingClient.shutdown();
+            } catch (InterruptedException e) {
+                Log.e(MainActivity.class.getSimpleName(), "Error", e);
+            }
+        }
+    }
+
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +198,7 @@ public class FourFragment extends Fragment {
         myFragmentView = inflater.inflate(R.layout.fragment_four, container, false);
         EnglishText = (TextView) myFragmentView.findViewById(R.id.EnglishText);
         FrenchText = (TextView) myFragmentView.findViewById(R.id.FrenhText);
+        mConsoleMsg = (TextView) myFragmentView.findViewById(R.id.mConsoleMsg);
         //editText = (EditText) myFragmentView.findViewById(R.id.editText);
      // text=list_of_wordsFrench[0];
                //editText.setFocusable(false);
@@ -121,12 +246,35 @@ public class FourFragment extends Fragment {
             }
         });
 
-        btnMicrophone = (ImageButton) myFragmentView.findViewById(R.id.btn_mic);
-        btnMicrophone.setOnClickListener(new View.OnClickListener() {
+        mBufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE, AudioFormat
+                .CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT) * 2;
 
+        mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE,
+                RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING,
+                mBufferSize);
+
+        initialize();
+
+        mRecordingBt = (ImageButton) myFragmentView.findViewById(R.id.btn_mic);
+        // mConsoleMsg = (TextView) view.findViewById(R.id.textView);
+        mRecordingBt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startSpeechToText();
+            public void onClick(View view) {
+                if (mIsRecording) {
+                    mIsRecording = false;
+                    mAudioRecord.stop();
+                    mStreamingClient.finish();
+                    //mRecordingBt.setText(R.string.start_recording);
+                } else {
+                    if (mAudioRecord.getState() == AudioRecord.STATE_INITIALIZED) {
+                       // mRecordingBt.setText(R.string.stop_recording);
+                        startRecording();
+                    } else {
+                        Log.i(this.getClass().getSimpleName(), "Not Initialized yet.");
+                    }
+                }
             }
         });
 
@@ -184,6 +332,7 @@ public class FourFragment extends Fragment {
                     for (int i = 0; i < initialText.length; i++) {
                         if (initialText[i].toString().compareTo(initialText[i].toString()) == 0) {
                             sb.setSpan(red, startPoint, startPoint + initialText[i].length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
 
                         } else {
                             sb.setSpan(green, startPoint, startPoint + initialText[i].length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
